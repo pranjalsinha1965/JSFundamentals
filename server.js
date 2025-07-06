8b3f0e663f68a313c1f171034fd5534ece4285d1
pranjalsinha1965 <pranjalsinha1965@gmail.com>
2025-07-07 01:03:11 +0530

    Keepd adding codes with example

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-
+const cors = require('cors');
+app.use(cors());
 // Static greeting endpoint
 app.get('/greeting', (req, res) => {
     const name = 'Alice';
